06951bb4417fc13bcec2352b33efe3f4def86c45
Yixin Yang <shawolyyx@gmail.com>
2022-07-24 21:50:49 +0000

    Update monitor interval to 0.5s

--- monitor.sh
+++ monitor.sh
@@ -2,6 +2,10 @@
 
 sandbox_type=$(cat `pwd`/default-ol/config.json | jq -r .sandbox)
 logfile=${sandbox_type}-mem-$(date +%FT%T).log
-interval=2
+interval=0.5
+
+if [[ ! -z $1 ]]; then
+    interval=$1
+fi
 
 watch -n $interval -t "free -m | awk 'NR==2 {print \$3}' | tee $logfile -a"
